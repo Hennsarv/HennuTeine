8a685d29447f3717b48ebf57b48add95923c0034
sarvi <sarvi@MLEKTOR>
2024-02-05 11:53:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HennuTeine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere");
+
+            int i = 77;
+            Console.WriteLine(i * i  );
+
+            Console.ReadKey();
+        }
+    }
+}
